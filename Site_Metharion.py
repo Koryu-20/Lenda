f8df2781f8b3cf87e565fdae38fd941bd112a558
Koryu-20 <tuguitohmartins3d@gmail.com>
2025-04-22 14:08:01 -0300

    Update Site_Metharion.py

--- Site_Metharion.py
+++ Site_Metharion.py
@@ -5,5 +5,5 @@ import streamlit.components.v1 as components
 with open("Site.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
-# Exibe o HTML no Streamlit
-components.html(html_content, height=700, scrolling=True)
+# Exibe o HTML no Streamlit com largura maior
+components.html(html_content, height=700, width=1200, scrolling=True)
